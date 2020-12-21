9be5687315e96eed464d01f583f7240d01945c75
Derrick Loo <derrickloo@hey.com>
2020-12-21 09:05:46 +0800

    Retrieve content via GraphQL from Contentful

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,18 +4,22 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/header'
 import './index.css'
+import Footer from '../components/Footer'
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: data.site.siteMetadata.description },
+        { name: 'keywords', content: data.site.siteMetadata.keywords },
       ]}
     />
     <Header />
     {children()}
+    <Footer data={data}>
+      Backgrounds made in Cinema 4D, iOS app in Swift, site in React. <a href="mailto:derrickloo@hey.com">Email us</a> to ask us anything. (C) 2020
+    </Footer>
   </div>
 )
 
@@ -30,6 +34,17 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        keywords
+        description
+      }
+    }
+    allContentfulLink(sort: {fields: [sortId], order: ASC }) {
+      edges {
+        node {
+          title
+          url
+          sortId
+        }
       }
     }
   }

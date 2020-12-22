4d3259168ac28c6ae33310d67958e3f589062ac8
Derrick Loo <derrickloo@hey.com>
2020-12-22 17:51:29 +0800

    Reinstall gatsby-source-contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,11 +12,11 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: 'gatsby-source-contentful', 
+      resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      }
+      },
     }
   ],
 }
